49d08aa5d38cb9b9d16158fe937f31110dc6b307
Nomenandrianina Antonio <nomenandrianinaantonio@gmail.com>
2023-07-24 11:48:41 +0300

    Creation project
    Creation project

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+require('dotenv').config();
+
+const cors = require('cors');
+var corsOptions = {
+    origin: "*"
+};
+const bodyParser = require("body-parser");
+const app = express();
+
+const port = process.env.PORT || 8000;
+var allowCrossDomain = function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain);
+app.use(cors({origin: "*"})); 
+app.use(bodyParser.json({limit: '50mb'})); 
+app.use(bodyParser.urlencoded({
+    limit: '50mb',    
+    extended: true
+}));
+
+app.listen(port, () => console.log('Server app listening on port ' + port));
